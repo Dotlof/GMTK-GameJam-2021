32e05a557913942e8507c5a75412ac72437a8bfb
Dotlof <51374399+Dotlof@users.noreply.github.com>
2021-06-11 23:42:52 +0200

    Added Prefab System/ Fixed Collision System/ Components now properly interact with each other

--- Assets/Scripts/scr_RepeaterPowerCheck.cs
+++ Assets/Scripts/scr_RepeaterPowerCheck.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_ComponentPowerCheck : MonoBehaviour
+public class scr_RepeaterPowerCheck : MonoBehaviour
 {
     public SpriteRenderer sr;
     public Sprite SPowered;
+    public Sprite SPoweredL;
+    public Sprite SPoweredR;
     public Sprite SUnpowered;
+
+    public bool powerR = false;
+    public bool powerL = false;
 
     public bool powered = false;
     public bool active = true;
@@ -14,16 +19,21 @@ public class scr_ComponentPowerCheck : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (powerL && powerR == true) powered = true;
+
+        if (powerL == true && powerR == false) sr.sprite = SPoweredL;
+        if (powerR == true && powerL == false) sr.sprite = SPoweredR;
+
         if (powered == true) sr.sprite = SPowered;
         else sr.sprite = SUnpowered;
     }
 
 
-    
-    }
+
+}
